590f02a1ad2127883eb4e064ad4ad8c636c4a83e
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2022-06-06 21:34:13 +0200

    Complete exercise

--- main.js
+++ main.js
@@ -1,76 +1,106 @@
 // start by creating functions for the following items
-
-// add
-function add(a, b){
-    return a + b;
+function add(a, b){ // add
+    return round((a + b), 5)
 }
 
-// subtract
-function subtract(a, b){
-    return a - b;
+function subtract(a, b){ // subtract
+    return round((a - b), 5)
 }
 
-// multiply
-function multiply(a, b){
-    return a * b;
+function multiply(a, b){ // multiply
+    return round((a * b), 5)
 }
 
-// divide
-function divide(a, b){
-    return a / b;
+function divide(a, b) { // divide
+    if (b == 0) { // error when divided by zero
+        return "...NO"
+    }
+    else {
+        return round((a / b), 5) 
+    }
 }
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-function operate(operator, a, b) {
-    return operator(a, b)
-}
+function operate(callback, a, b) {
+    return callback == "add" ? add(a, b)
+    : callback == "subtract" ? subtract(a, b)
+    : callback == "multiply" ? multiply(a, b) 
+    : callback == "divide" ? divide(a, b)
+    : "error";
+   }
 
 // Create the functions that populate the display when you click the number buttons… 
 // you should be storing the ‘display value’ in a variable somewhere for use in the next step.
 
-let displayNum = ""; // number that goes on the display
-let displayOperator = ""; // operator that goes on the display
+let displayNum = ""; // value that goes on the display
+let num1;
+let num2;
+let operator;
 
-let firstNumber = 0;
+document.getElementById("reverse").addEventListener('click', event => { // when reverse button is pressed
+    if (Number(displayNum) > 0) { // if number on the display is positive..
+    displayNum = displayNum.replace (/^/,'-'); //.. add minus
+    document.getElementById("display").innerHTML = displayNum;
+    }
 
-document.querySelectorAll('.numbers').forEach(item => { // when number is pressed, store it to variable and add to display
+    else { // if number on the display is negative..
+        displayNum = displayNum.replace ('-',''); //.. remove minus
+        document.getElementById("display").innerHTML = displayNum;
+    }
+})
+
+document.querySelectorAll('.numbers').forEach(item => { // when number button is clicked    
     item.addEventListener('click', event => {
-        displayNum += event.target.id;
-        addToDisplay(displayNum);
+        if (event.target.id == "." && displayNum.includes(".")) { // to prevent more than one decimal points
+            return
+        }
+        else {
+        displayNum += event.target.id; // add that number into variable displayNum
+        document.getElementById("display").innerHTML = displayNum; // show displayNum on display
+        }
     })
 })
 
-function addToDisplay(param){ // add parameter to display
-    document.getElementById("display").innerHTML = param;
-}
-
-// store the first number that is input into the calculator when a user presses an operator, 
-// and also save which operation has been chosen and then operate() on them when the user presses the “=” key.
-
-document.querySelectorAll('.operators').forEach(item => { 
+document.querySelectorAll('.operators').forEach(item => { // when operator is clicked
     item.addEventListener('click', event => {
-        displayOperator = event.target.id;
-        firstNumber = Number(displayNum);
-    })
+            if(operator == undefined) { // if operator is undefined (= this is first time it is pressed)
+                operator = event.target.id; // store clicked operator into variable operator
+                num1 = Number(displayNum); // store number on display into num1 variable
+                displayNum = ""; // clear display
+            }
+            else if (operator !== undefined) { // if operator is not undefined (= this is not first time it is pressed)
+                num2 = Number(displayNum); // store number on display into num2 variable 
+                num1 = operate(operator, num1, num2); // num1 will be calculation based on operator, num1 and num2
+                                                                // operator and num1 were stored earlier
+                operator = event.target.id; // store new operator based on pressed operator button
+                document.getElementById("display").innerHTML = num1; // show num1 (result of calculation) on display
+                displayNum = ""; // clear display
+            } 
+
+        })
 })
 
-
-
-/*
-
-
-document.getElementById("equals").addEventListener('click', event => {
-    addToDisplay(event.target.id);
+document.getElementById("equals").addEventListener('click', event => { // when equals is pressed
+    if(operator == undefined && num2 == undefined) { // if operator or num2 has not been set, equals button should not do anything
+        return
+    }
+    else if(operator !== undefined) { // if operator has been set, equals button is functioning
+            num2 = Number(displayNum); // store number on display into num2 variable 
+            document.getElementById("display").innerHTML = operate(operator, num1, num2); // show result of calculation on display
+    }  
     })
 
-function addToDisplay(num){
-    document.getElementById("display").innerHTML += num;
+    document.getElementById("clear").addEventListener('click', event => { // when clear is pressed
+        resetCalculator(); // call function to reset calculator
+})
+
+function resetCalculator() { // reset calculator - set variables to undefined and clear displayNum & display
+    num1 = undefined;
+    num2 = undefined;
+    operator = undefined;
+    displayNum = ""; // clear display
+    document.getElementById("display").innerHTML = displayNum; // add number on display
 }
-*/
 
-/*
-
-document.getElementById("clear").addEventListener('click', event => {
-    addToDisplay(event.target.id);
-    })
- */
+// function to round up the results of math functions
+const round = (n, d) => Math.round(n * Math.pow(10, d)) / Math.pow(10, d)
